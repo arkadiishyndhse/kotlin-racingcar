a5e4bf910d6824bafd86accf4be552b896f30d6e
arkadiishyndhse <143792532+arkadiishyndhse@users.noreply.github.com>
2025-03-25 11:23:39 +0100

    Step 3 - Car Racing (#1801)
    * test: Person class
    
    * test: Person class
    
    * docs: Readme
    
    * feat: input validation
    
    * feat: arithmetic operations
    
    * feat: execution
    
    * feat: error handling
    
    * feat: unit tests
    
    * refactor: project restructure
    
    * refactor: enum class operator
    
    * test: enum class operator
    
    * refactor: style
    
    * docs: Readme
    
    * feat: input validation
    
    * feat: arithmetic operations
    
    * feat: execution
    
    * feat: error handling
    
    * feat: unit tests
    
    * refactor: project restructure
    
    * refactor: enum class operator
    
    * test: enum class operator
    
    * refactor: style
    
    * feat: car class
    
    * feat: race class
    
    * feat: display race
    
    * test: car class

---
+++ src/test/kotlin/carracing/CarTest.kt
@@ -0,0 +1,31 @@
+package carracing
+
+import io.mockk.every
+import io.mockk.mockkObject
+import kotlin.random.Random
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class CarTest {
+    @ParameterizedTest
+    @ValueSource(ints = [4, 5, 6, 7, 8, 9])
+    fun `car should move when random number is 4 or greater`(randomValue: Int) {
+        val car = Car()
+        mockkObject(Random)
+        every { Random.nextInt(10) } returns randomValue
+        car.move()
+        assertEquals("-", car.toString())
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, 1, 2, 3])
+    fun `car should not move when random number is less than 4`(randomValue: Int) {
+        val car = Car()
+        mockkObject(Random)
+        every { Random.nextInt(10) } returns randomValue
+
+        car.move()
+        assertEquals("", car.toString())
+    }
+}
